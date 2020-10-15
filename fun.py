8074a42fbab5c2c2dcfc76bc4917593f39d39619
Deepak Mishra <40403460+iemdpk@users.noreply.github.com>
2020-10-15 18:44:46 +0530

    Add files via upload

---
+++ fun.py
@@ -0,0 +1,19 @@
+def helloWorld():
+    print("This is Inside THe function")
+
+def add(a,b):
+    c = int(a) + int(b) 
+    print("This is THe total " + str(c))
+
+
+
+print("==================================================")
+print("Deepak")
+helloWorld()
+
+#input()
+first = input();
+second = input();
+add(first,second)
+
+                    
